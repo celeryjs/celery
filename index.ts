49b519697aa0654ceb51f46bebdd3fff64e7e584
Socheat Sok <socheatsok78@gmail.com>
2023-08-23 18:57:32 +0700

    Implement `CeleryPromise.finally`

--- index.ts
+++ index.ts
@@ -29,6 +29,14 @@ interface CeleryPromise<T> {
      * @returns A Promise for the completion of the callback.
      */
     catch<TResult = never>(onrejected?: ((reason: CeleryError) => TResult | PromiseLike<TResult>) | undefined | null): CeleryPromise<T | TResult>;
+
+    /**
+     * Attaches a callback that is invoked when the Promise is settled (fulfilled or rejected). The
+     * resolved value cannot be modified from the callback.
+     * @param onfinally The callback to execute when the Promise is settled (fulfilled or rejected).
+     * @returns A Promise for the completion of the callback.
+     */
+    finally(onfinally?: (() => void) | undefined | null): CeleryPromise<T>
 }
 
 /**
