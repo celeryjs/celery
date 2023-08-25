cd7d702dc35c87d818d95da0e905f4acac00ea16
Socheat Sok <socheatsok78@gmail.com>
2023-08-25 18:41:48 +0700

    Update global Celery instance and exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import { Celery } from './Celery'
+
 export * from './types'
 export * from './Celery'
 export * from './CeleryCore'
 export * from './CeleryError'
+
+/**
+ * Global Celery instance
+ */
+export const celery = new Celery()
+
+/**
+ * Default global Celery instance
+ */
+export default celery
