cb997bf853d6b7f310dd7a98ac95cf431a2a1f70
Socheat Sok <socheatsok78@gmail.com>
2023-08-25 18:38:44 +0700

    Create abstract `CeleryInstance`

--- src/Celery.ts
+++ src/Celery.ts
@@ -1,23 +1,16 @@
-import { CeleryCore, CeleryCoreOptions } from "./CeleryCore";
+import { CeleryInstance } from "./CeleryInstance";
 import { ChildCeleryInstance } from "./ChildCeleryInstance";
-import type { CeleryPayloadRequest, CeleryRequest } from "./types";
+import type { CeleryCoreOptions } from "./CeleryCore";
 
 /**
  * Celery HTTP Client
  */
-export class Celery extends CeleryCore {
-    /**
-     * Create a new Celery instance
-     */
-    static create(options?: CeleryCoreOptions) {
-        return new this(options)
-    }
-    
+export class Celery extends CeleryInstance {
     /**
      * Create a new Celery instance
      */
     public create(options?: CeleryCoreOptions) {
-        return new Celery(options)
+        return new CeleryInstance(options)
     }
 
     /**
@@ -27,68 +20,5 @@ export class Celery extends CeleryCore {
      */
     public extends(options?: CeleryCoreOptions) {
         return new ChildCeleryInstance(this, options)
-    }
-
-    /**
-     * Send a HEAD request to the given URL
-     * 
-     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/HEAD)
-     */
-    public head<Params = any>(url: string, config?: CeleryRequest<Params>) {
-        return this.$request(Object.assign({}, config, { method: 'HEAD', url: url }));
-    }
-
-    /**
-     * Send a OPTIONS request to the given URL
-     * 
-     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/OPTIONS)
-     */
-    public options<Response = any, Params = any>(url: string, config?: CeleryRequest<Params>) {
-        return this.$request<Response>(Object.assign({}, config, { method: 'OPTIONS', url: url }));
-    }
-
-    /**
-     * Send a GET request to the given URL
-     * 
-     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/GET)
-     */
-    public get<Response = any, Params = any>(url: string, config?: CeleryRequest<Params>) {
-        return this.$request<Response>(Object.assign({}, config, { method: 'GET', url: url }));
-    }
-
-    /**
-     * Send a POST request to the given URL
-     * 
-     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST)
-     */
-    public post<Response = any, Payload = any, Params = any>(url: string, config: CeleryPayloadRequest<Payload, Params>) {
-        return this.$request<Response, Payload>(Object.assign({}, config, { method: 'POST', url: url }));
-    }
-
-    /**
-     * Send a PUT request to the given URL
-     * 
-     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/PUT)
-     */
-    public put<Response = any, Payload = any, Params = any>(url: string, config: CeleryPayloadRequest<Payload, Params>) {
-        return this.$request<Response, Payload>(Object.assign({}, config, { method: 'PUT', url: url }));
-    }
-
-    /**
-     * Send a PATCH request to the given URL
-     * 
-     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/PATCH)
-     */
-    public patch<Response = any, Payload = any, Params = any>(url: string, config: CeleryPayloadRequest<Payload, Params>) {
-        return this.$request<Response, Payload>(Object.assign({}, config, { method: 'PATCH', url: url }));
-    }
-
-    /**
-     * Send a DELETE request to the given URL
-     * 
-     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE)
-     */
-    public delete<Response = any, Payload = any, Params = any>(url: string, config: CeleryPayloadRequest<Payload, Params>) {
-        return this.$request<Response, Payload>(Object.assign({}, config, { method: 'DELETE', url: url }));
     }
 }
---
+++ src/CeleryInstance.ts
@@ -0,0 +1,70 @@
+import { CeleryCore } from "./CeleryCore";
+import type { CeleryPayloadRequest, CeleryRequest } from "./types";
+
+/**
+ * Celery HTTP Client
+ */
+export class CeleryInstance extends CeleryCore {
+    /**
+     * Send a HEAD request to the given URL
+     * 
+     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/HEAD)
+     */
+    public head<Params = any>(url: string, config?: CeleryRequest<Params>) {
+        return this.$request(Object.assign({}, config, { method: 'HEAD', url: url }));
+    }
+
+    /**
+     * Send a OPTIONS request to the given URL
+     * 
+     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/OPTIONS)
+     */
+    public options<Response = any, Params = any>(url: string, config?: CeleryRequest<Params>) {
+        return this.$request<Response>(Object.assign({}, config, { method: 'OPTIONS', url: url }));
+    }
+
+    /**
+     * Send a GET request to the given URL
+     * 
+     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/GET)
+     */
+    public get<Response = any, Params = any>(url: string, config?: CeleryRequest<Params>) {
+        return this.$request<Response>(Object.assign({}, config, { method: 'GET', url: url }));
+    }
+
+    /**
+     * Send a POST request to the given URL
+     * 
+     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST)
+     */
+    public post<Response = any, Payload = any, Params = any>(url: string, config: CeleryPayloadRequest<Payload, Params>) {
+        return this.$request<Response, Payload>(Object.assign({}, config, { method: 'POST', url: url }));
+    }
+
+    /**
+     * Send a PUT request to the given URL
+     * 
+     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/PUT)
+     */
+    public put<Response = any, Payload = any, Params = any>(url: string, config: CeleryPayloadRequest<Payload, Params>) {
+        return this.$request<Response, Payload>(Object.assign({}, config, { method: 'PUT', url: url }));
+    }
+
+    /**
+     * Send a PATCH request to the given URL
+     * 
+     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/PATCH)
+     */
+    public patch<Response = any, Payload = any, Params = any>(url: string, config: CeleryPayloadRequest<Payload, Params>) {
+        return this.$request<Response, Payload>(Object.assign({}, config, { method: 'PATCH', url: url }));
+    }
+
+    /**
+     * Send a DELETE request to the given URL
+     * 
+     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE)
+     */
+    public delete<Response = any, Payload = any, Params = any>(url: string, config: CeleryPayloadRequest<Payload, Params>) {
+        return this.$request<Response, Payload>(Object.assign({}, config, { method: 'DELETE', url: url }));
+    }
+}
