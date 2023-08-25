c8b4e7ac577ff198b3370cec7086213731603ee2
Socheat Sok <socheatsok78@gmail.com>
2023-08-25 12:37:57 +0700

    Implement `withFirstFound` utility function

--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,16 @@
  * @returns Get the default origin
  */
 export const getDefaultOrigin = () => (typeof window !== 'undefined' && window.location) ? new URL(window.location.origin) : undefined
+
+/**
+ * Get the first found value
+ * @param args 
+ * @returns 
+ */
+export function withFirstFound<T>(...args: T[]) {
+    for (const arg of args) {
+        if (arg !== undefined) {
+            return arg
+        }
+    }
+}
